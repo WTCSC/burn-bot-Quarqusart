d7dc1717c04b579d7ba9ea9a79d30a1a67bd876c
Quarqusart <dylan.soule@icloud.com>
2025-08-28 12:42:35 -0600

    Updated Burn Bot

--- .github/BurnBot.py
+++ .github/BurnBot.py
@@ -31,7 +31,7 @@ printCool("Could you please give me the hospital you were born in next?")
 PlaceOfOrigin = input("")
 addNo(PlaceOfOrigin)
 
-printCool("How about your first pet?")
+printCool("How about your first pet? What was their name?")
 Pet = input("")
 addNo(Pet)
 
@@ -63,15 +63,19 @@ printCool("Just one more, what's your social security number?")
 SocialCode = input("")
 addNo(SocialCode)
 
-numNos = 0
 printCool(f"Alright, lets see what we got here {Name}, Age {Age}, and is {Height}")
 time.sleep(0.5)
 
 printCool(f"Dang, your parents really messed you up didn't they? Not only giving you the name {Name}, \nbut aslo giving you that name when you were born, DANG that sucks")
 if int(Age) < 20:
-    printCool(f"Speaking of your age, you're really that young huh. Can't image what it must be like to be living as a baby during todays day and age. Like wow, do you even know what a CD is")
+    printCool(f"Speaking of your age, you're really that young huh. Can't image what it must be like to be living as a baby during todays day and age. Like wow, do you even know what a CD is??")
 else:
     printCool(f"Speaking of age, it seems we've got a grandpa here. Are you still using VHS tapes to watch movies? It must be cool to have been alive during the roaring 20's")
+printCool("I'm just gonna skip over your height, becuase that doesn't really matter does it? Or at least you can't control it, unlike your name or age.")
+time.sleep(0.5)
 
-
-
+printCool("Okay, now lets see if you were dim enough to believe me earlier and you gave me all your accounts, or if you at least have something knocking around in the noggin of yours")
+if NoCounter > 5:
+    printCool("Dannggg, we really got a braniac here, you said no to my inquires {NoCounter} times. I really don't know what to do with you, I guess that I have to leave you here.\nWell then, good bye Mr. Cybersecurity, I guess I won't be taking over all your accounts, which knowing you probably all have different passwords, you're so special.")
+else:
+    printCool("Well, lookie here, you really gave me everything didn't you. If I were'nt just a a python script, you'd be done, identity taken, I could probably become you if I tried hard enough.")
